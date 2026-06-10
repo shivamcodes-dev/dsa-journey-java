7652d94637fa8b29303abb29e3b138db5305c1df
Shivam Raj <srshivam620@gmail.com>
2026-06-10 07:09:32 +0530

    Added Basic folder

--- Basic/Ch07.java
+++ Basic/Ch07.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
 public class Ch07 {
-    public static void main(String args[]){
-       Scanner sc = new Scanner(System.in);
-       System.out.print("Enter First no. ");
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First no. ");
         int a = sc.nextInt();
-        System.out.print("Enter Second no. ");
+        System.out.print("Enter Second number. ");
         int b = sc.nextInt();
 
-        if(a > b){
+        if (a > b) {
             System.out.println("Largest is " + a);
-        }
-        else{
+        } else {
             System.out.println("Largest is " + b);
         }
     }
